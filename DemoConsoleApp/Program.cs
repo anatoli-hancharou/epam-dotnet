b0cec51019c390963bf7679cac3876aec2f06586
My Name <tolushasun@mail.ru>
2021-02-11 15:28:24 +0300

    Add ruleset file and configure DemoConsoleApp project to use the code-analisys.ruleset.

--- DemoConsoleApp/Program.cs
+++ DemoConsoleApp/Program.cs
@@ -4,8 +4,6 @@ namespace DemoConsoleApp
 {
     class Program
     {
-        [method: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1303", Justification = "Should be turned off.")]
-        [method: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1801", Justification = "Code that is using args will be added later.")]
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
