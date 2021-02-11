b0aadd98ed0a1c91e6fdd9c2ca1c9de22176a2dd
My Name <tolushasun@mail.ru>
2021-02-11 17:36:59 +0300

    Add .editorconfig file.

--- DemoConsoleApp/Program.cs
+++ DemoConsoleApp/Program.cs
@@ -2,8 +2,15 @@
 
 namespace DemoConsoleApp
 {
+    /// <summary>
+    /// An application entry point.
+    /// </summary>
     public static class Program
     {
+        /// <summary>
+        /// Starts an application.
+        /// </summary>
+        /// <param name="args">Application arguments.</param>
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
